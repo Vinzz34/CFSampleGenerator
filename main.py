2c82baf901705393fd3221c0d8414949982b50a6
Vinzz34 <vinaygn29@gmail.com>
2021-04-07 17:24:49 +0530

    Updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from termcolor import colored
 import requests
 import os
 import time
@@ -7,20 +8,26 @@ import sys
 #---Global---
 contestid=sys.argv[1]
 url="https://codeforces.com/contest/{}".format(str(contestid))
-print("Fetching "+url+"...")
+print("Fetching "+url+"... \n")
 page=requests.get(url).text
 soup=BeautifulSoup(page,'lxml')
 parentpath="/home/vinzz/c++/cf"
 
+tables = soup.findAll('table')
+contest_name = tables[0].find('a').text.strip()
+print(contest_name+"\n")
+
 #create folder
 path=os.path.join(parentpath,str(contestid))
-print("Creating directory "+str(contestid)+" ...")
+print("Creating directory "+str(contestid)+" ... \n")
 os.mkdir(path)
 
 #template
 templatef=open('/home/vinzz/Templates/template.txt',"r")
 template=templatef.read().strip()
 templatef.close()
+
+time.sleep(3)
 
 #problem create
 path=path+"/"
@@ -34,10 +41,11 @@ def probcreate(prob_id):
 
 #get input and output
 def io(prob_id):
-    print("Creating input output for "+prob_id+" ...")
     ifile=prob_id+".in"
+    print("Creating "+ifile+" ...")
     ifile=path+ifile
     ofile=prob_id+".out"
+    print("Creating "+ofile+" ...")
     ofile=path+ofile
     wf=open(ifile,"a")
     of=open(ofile,"a")
@@ -62,12 +70,15 @@ def io(prob_id):
 #main
 
 problems=soup.find('div',class_='datatable').find('table').findAll('a')
+print("Generating Problems ...")
 
 for i in range(len(problems)):
     if(i%4!=0): continue
     prob_id=problems[i].text.strip()
     probcreate(prob_id)
     io(prob_id)
+    print("\n")
 
 print("Almost done ...")
-print("Generated codeforces "+str(contestid)+".")
+time.sleep(3)
+print colored("Successfully Generated codeforces "+str(contestid)+".",'')
